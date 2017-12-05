66b6ae667391f0956fc4e27e6c405b6f249452fe
steve-horan <stephen.horanjr@pjm.com>
2017-12-04 21:43:42 -0500

    Adding Day 3 Solution 1 Python

--- 02-go/02-01/main.go
+++ 02-go/02-01/main.go
@@ -13,16 +13,16 @@ func main() {
 	b := bufio.NewScanner(f)
 	var s int
 	for b.Scan() {
-		line := strings.Split(b.Text(), "\t")
+		line := strings.Split(b.Text(), "\t") // Here we split each line by tabs and create a new slice of strings
 		a := func() []int {
 			s := []int{}
 			for _, i := range line {
-				newint, _ := strconv.Atoi(i)
+				newint, _ := strconv.Atoi(i) // We then convert our []string to []int
 				s = append(s, newint)
 			}
 			return s
 		}()
-		l := func() int {
+		l := func() int { //Function to get the lowest number in slice
 			var m int
 			for _, i := range a {
 				if m == 0 {
@@ -33,7 +33,7 @@ func main() {
 			}
 			return m
 		}()
-		h := func() int {
+		h := func() int { //Function to get the highest number in a slice
 			var m int
 			for _, i := range a {
 				if m == 0 {
---
+++ 03-python/03-01/main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+moves = {"right": {"x": "+", "y": None},  "up": {"x": None, "y": "+"}, "left": {"x": "-", "y": None}, "down": {"x": None, "y": "-"}}
+order = ("right", "up", "left", "down")
+
+def move(action, pos):
+    m = moves[order[action]]
+    print(m, order[action])
+    if m["x"] == "+":
+        pos["x"] = pos["x"] + 1
+    if m["x"] == "-":
+        pos["x"] = pos["x"] - 1
+    if m["y"] == "+":
+        pos["y"] = pos["y"] + 1
+    if m["y"] == "-":
+        pos["y"] = pos["y"] - 1
+    
+    return pos
+
+def main():
+    pos = {"x": 0, "y": 0}
+    num = 1
+    index = 0
+    factor = 1
+    tick = 1
+    while num <= 10:
+        for _ in range(factor):
+            pos = move(index, pos)
+            num = num + 1
+            print(pos, num)
+
+        if index == 3:
+            index = 0
+        else:
+            index = index + 1
+        
+        if tick % 2 == 0:
+            factor = factor + 1
+
+        tick = tick + 1
+
+main()
