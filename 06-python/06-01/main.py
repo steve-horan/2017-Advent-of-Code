723ee2779b1692cc3378e5cd85fd35c6e4bf5ff1
steve-horan <stephen.horanjr@pjm.com>
2017-12-07 18:50:00 -0500

    Adding Day 6 Solution 2 python.

--- 06-python/06-01/main.py
+++ 06-python/06-01/main.py
@@ -7,28 +7,34 @@ with open("main.txt") as f:
 def high_bank():
     return banks.index(max(banks))
 
-print(banks)
 configs_len = len(banks) - 1
 configs = []
 num = 0
+found = False
+dup = ""
+found_num = 0
 while True:
-    #configs.append(' '.join(str(x) for x in banks))
     bank_val = banks[high_bank()]
     start = high_bank()
     banks[high_bank()] = 0
     while bank_val != 0:
         if start >= configs_len:
             start = -1
-        print(start, configs_len, len(banks))
         start = start + 1
         banks[start] = banks[start] + 1
         bank_val = bank_val - 1
     configs.append(''.join(str(x) for x in banks))
-    print(num, configs[-1])
     num = num + 1
-    if len(configs) != len(set(configs)):
-        print(num)
-        sys.exit(0)
+    if found:
+        if configs[-1:] == dup:
+            print(num - found_num)
+            sys.exit(0)
+    elif len(configs) != len(set(configs)):
+            found = True
+            dup = configs[-1:]
+            found_num = num
+
+
 
         
 
